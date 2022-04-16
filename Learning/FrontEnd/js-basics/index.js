fe48b90023e8a74d9705fd95a6920ab72d8f19ef
SaranSaiChava <saran.chava143@gmail.com>
2022-04-16 14:44:48 +0530

    Added learnings and project : snake game

---
+++ Learning/FrontEnd/js-basics/index.js
@@ -0,0 +1,36 @@
+
+
+// Objects
+
+/*
+let person = {
+    name: 'Mosh',
+    age: 30
+};
+
+//Dot Notation
+person.name= 'Saran';
+console.log(person.name);
+
+//Bracket Notation
+
+person['age'] = 21;
+
+console.log(person)
+*/
+
+// Arrays
+
+// Array is an object in js
+/*
+let selectedColors = ['red','blue'];
+console.log(selectedColors);
+*/
+
+// Function
+
+function greet(name='World'){
+    console.log('Hello!! '+name);
+}
+
+greet('Saran');
---
+++ Projects/Snake/snakeLogic.js
@@ -0,0 +1,18 @@
+
+class Snake{
+    constructor(startPos=[0,0]){
+        this.pos=[];
+        this.pos[0]=startPos[0];
+        this.pos[1]=startPos[1];
+    }
+
+    method; newPos(newPos) {
+        
+    }
+}
+
+class Game{
+    constructor(gameStatus="new"){
+        this.gameStatus=gameStatus;
+    }
+}
